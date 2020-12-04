b7e0f5565dbf515869b3bad79e0cb8b83dd44822
Nikos Mpakaris <57401398+mpakaris@users.noreply.github.com>
2020-12-04 18:37:15 +0100

    Update taskService.js

--- backend/src/services/taskService.js
+++ backend/src/services/taskService.js
@@ -27,19 +27,7 @@ export const taskService = {
     this.saveTasks(returnArray)
     return returnArray
   },
-
-  async saveTasks(jsonData) {
-    let jsonContent = JSON.stringify(jsonData);
-    console.log(jsonContent);
-    fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
-      if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-      }
-      console.log("JSON file has been saved.");
-    });
-  },
-
+  
   async updateStatus(taskName) {
     let response = JSON.parse(fs.readFileSync("output.json", 'utf8'))
     console.log("TaskNAME:", taskName)
@@ -53,4 +41,18 @@ export const taskService = {
     this.saveTasks(returnArray)
     return returnArray;
   },
+
+  async saveTasks(jsonData) {
+    let jsonContent = JSON.stringify(jsonData);
+    console.log(jsonContent);
+    fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
+      if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+      }
+      console.log("JSON file has been saved.");
+    });
+  },
+
+  
 };
