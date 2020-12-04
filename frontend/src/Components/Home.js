e95d4341466505de04c0c7ab11786ecd7201f39b
Nikos Mpakaris <nikos.mpakaris@gmail.com>
2020-12-04 18:02:12 +0100

    implemented all functionalities

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
-import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Form, Row, Spinner } from 'react-bootstrap'
 import Header from '../../src/assets/header.PNG'
 import styles from './Home.module.css'
 
 const Home = () => {
 
-  const toDos = [
-    {
-      name: 'Task 1',
-      status: true
-    },
-    {
-      name: 'Task 2',
-      status: false
-    },
-    {
-      name: 'Task 3',
-      status: true
-    }
-  ]
-
-  const [tasks, setTasks] = useState(toDos)
+  const [tasks, setTasks] = useState([])
   const [newTask, setNewTask] = useState('')
+  const [spinner, setSpinner] = useState(false)
 
-  const changeStatus = (taskName) => {
-    let tasksChange = [...tasks];
-    tasksChange.map(task => {
-      if (task.name === taskName) {
-        task.status = !task.status
-      }
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getTasks()
+      setTasks(result.data)
+    };
+    fetchData()
+  }, []);
+
+  const getTasks = async () => {
+    setSpinner(true)
+    let response;
+    await axios({
+      url: 'http://localhost:5000/api/getTasks',
+      method: 'get',
+    }).then(res => {
+      response = res;
+      setSpinner(false)
     })
-    setTasks(tasksChange)
+    return response;
   }
 
-  const deleteTask = (taskName) => {
-    let newTaskArray = tasks.filter(task => {
-      if (task.name !== taskName) {
-        return task
-      }
+  const addTask = async (task) => {
+    setSpinner(true)
+    await axios({
+      url: 'http://localhost:5000/api/addTask',
+      method: 'post',
+      data: task
+    }).then(res => {
+      setTasks(res.data);
+      setSpinner(false)
     })
-    setTasks(newTaskArray)
   }
 
   const handleChange = (e) => {
@@ -49,32 +50,61 @@ const Home = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("submit works", newTask)
-    let taskList = [...tasks]
-    taskList.push({
+    addTask({
       name: newTask,
       status: false
     })
-    setTasks(taskList)
     setNewTask('')
+  }
+
+  const deleteTask = async (taskName) => {
+    setSpinner(true)
+    await axios({
+      url: 'http://localhost:5000/api/deleteTask',
+      method: 'delete',
+      data: {
+        name: taskName
+      }
+    }).then(res => {
+      setTasks(res.data);
+      setSpinner(false)
+    })
+  }
+
+  const changeStatus = async (taskName) => {
+    setSpinner(true)
+    await axios({
+      url: 'http://localhost:5000/api/changeStatus',
+      method: 'put',
+      data: {
+        name: taskName
+      }
+    }).then(res => {
+      setTasks(res.data);
+      setSpinner(false)
+    })
   }
 
   return (
     <div className={styles.home}>
+      {
+        spinner && <Spinner animation="border" role="status" style={{ position: 'absolute', left: '45%', top: '50%' }}>
+        </Spinner>
+      }
       <img src={Header} alt="header" />
       <p className='h3 text-center my-3'>TASK MASTER</p>
       {tasks && tasks.map(task => {
         return (
           <Container className={`border py-3 my-3 ${task.status ? "border-success" : "border-warning"}`} key={tasks.indexOf(task)}>
             <Row>
-              <Col xs={2} md={2} >
+              <Col xs={2} md={2} className="d-flex align-items-center">
                 <Form.Check checked={task.status} aria-label="option" onChange={() => changeStatus(task.name)} />
               </Col>
-              <Col xs={5} md={7} >
+              <Col xs={5} md={7} className="d-flex align-items-center">
                 {task.name}
               </Col>
-              <Col xs={5} md={3} >
-                <p className={styles.cursor} onClick={() => deleteTask(task.name)}>Löschen</p>
+              <Col xs={5} md={3} className="d-flex justify-content-center align-items-center">
+                <p style={{ cursor: 'pointer' }} onClick={() => deleteTask(task.name)}>Löschen</p>
               </Col>
             </Row>
           </Container>
